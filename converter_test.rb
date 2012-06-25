933d8a9295f266007137f676c87141a570533702
David <ddd1600@gmail.com>
2012-06-25 16:00:17 -0600

    class variables versus instance variables versus local thing

--- converter_test.rb
+++ converter_test.rb
@@ -5,17 +5,20 @@ class TestConverter < Test::Unit::TestCase
 
   #@cv = Convert.new
   #instantiated the class in each method instead of here
+	#I'm told I could've use @@cv and been successful.
+	#I'm also told I have a flawed understanding of instance variables.
+	#@@cv = Convert.new, yeah that works.
 
   def test_celsius
     cv = Convert.new
-    assert_equal(100.0, cv.celsius(212))
-    assert_equal(0, cv.celsius(32))
+    assert_equal(100.0, @@cv.celsius(212))
+    assert_equal(0, @@cv.celsius(32))
   end
   
   def test_fahrenheit
     cv = Convert.new
-    assert_equal(212, cv.fahrenheit(100))
-    assert_equal(32, cv.fahrenheit(0))
+    assert_equal(212, @@cv.fahrenheit(100))
+    assert_equal(32, @@cv.fahrenheit(0))
   end
   
 end
